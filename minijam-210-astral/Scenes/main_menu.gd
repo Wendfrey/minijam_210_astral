f5e04bfe84e2ed9abc5894a86c5f5e75fdacc94e
Wendfrey <toni.cano.redondo@gmail.com>
2026-05-09 20:59:46 +0200

    play button works, fixed options showing up at startup

--- minijam-210-astral/Scenes/main_menu.gd
+++ minijam-210-astral/Scenes/main_menu.gd
@@ -6,7 +6,6 @@ extends Control
 func _on_btn_exit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_btn_options_pressed() -> void:
 	options_screen.show()
 	v_box_container.hide()
@@ -14,3 +13,6 @@ func _on_btn_options_pressed() -> void:
 func _on_options_screen_close() -> void:
 	options_screen.hide()
 	v_box_container.show()
+
+func _on_btn_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Nivel.tscn")
