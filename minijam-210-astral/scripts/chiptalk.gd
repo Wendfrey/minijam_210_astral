f13b30d73ea3fae3e11d53f9b6335b0f3f97d17c
Marcopole11 <nicomarcopernic@gmail.com>
2026-05-09 22:21:58 +0200

    mas transiciones chulas

--- minijam-210-astral/scripts/chiptalk.gd
+++ minijam-210-astral/scripts/chiptalk.gd
@@ -2,6 +2,7 @@ extends Label
 
 @export var textLines: Array[String]
 @export var chiptalkAnim: AnimationPlayer
+@export var sceneTransition: AnimationPlayer
 
 var lineCount = 0;
 
@@ -11,10 +12,18 @@ func _on_chiptalk_anim_animation_finished(anim_name):
 		lineCount +=1;
 		chiptalkAnim.play(anim_name)
 	else:
-		nextScene();
+		endScene();
 
 func _on_skip_button_pressed():
-	nextScene();
+	endScene();
 
-func nextScene():
-	get_tree().change_scene_to_file("res://Scenes/Nivel.tscn")
+func endScene():
+	chiptalkAnim.stop()
+	sceneTransition.play("out");
+	
+
+func _on_scene_transition_animation_finished(anim_name):
+	if anim_name == "start":
+		chiptalkAnim.play("chiptalkFade");
+	elif anim_name == "out":
+		get_tree().change_scene_to_file("res://Scenes/Nivel.tscn")
