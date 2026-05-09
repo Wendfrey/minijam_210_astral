2bdbdc65a9697aadd09be7b47d13c831f1edda26
Wendfrey <toni.cano.redondo@gmail.com>
2026-05-09 20:36:44 +0200

    main menu pause is transparent now

--- minijam-210-astral/Scenes/main_menu.gd
+++ minijam-210-astral/Scenes/main_menu.gd
@@ -1,9 +1,16 @@
 extends Control
 
+@onready var options_screen: Control = $OptionsScreen
+@onready var v_box_container: VBoxContainer = $VBoxContainer
 
 func _on_btn_exit_pressed() -> void:
 	get_tree().quit()
 
 
 func _on_btn_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/OptionsScreen.tscn")
+	options_screen.show()
+	v_box_container.hide()
+
+func _on_options_screen_close() -> void:
+	options_screen.hide()
+	v_box_container.show()
--- minijam-210-astral/Scenes/options_screen.gd
+++ minijam-210-astral/Scenes/options_screen.gd
@@ -1,6 +1,7 @@
 extends Control
 
+signal close
 
 func _on_btn_main_menu_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	close.emit()
