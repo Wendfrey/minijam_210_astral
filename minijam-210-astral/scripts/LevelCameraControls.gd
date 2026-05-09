d1bbd91cad250737437b4fcc7387f791db920401
Marcopole11 <nicomarcopernic@gmail.com>
2026-05-09 20:37:27 +0200

    ahora si se ve bien!!!!

--- minijam-210-astral/scripts/LevelCameraControls.gd
+++ minijam-210-astral/scripts/LevelCameraControls.gd
@@ -7,3 +7,10 @@ extends CanvasLayer;
 func _on_button_pressed():
 	CameraAnimation.play("ToWindow")
 	pass # Replace with function body.
+
+
+func _on_back_button_pressed():
+	CameraAnimation.play_backwards("ToWindow")
+	await CameraAnimation.animation_finished
+	CameraAnimation.play("idle")
+	pass # Replace with function body.
