571dd592641f94f8a5d8414af71164a6d2dc0e7a
Marcopole11 <nicomarcopernic@gmail.com>
2026-05-10 23:23:29 +0200

    fix

--- minijam-210-astral/scripts/statusCheck.gd
+++ minijam-210-astral/scripts/statusCheck.gd
@@ -2,8 +2,6 @@ extends Panel
 
 @export var indicators:Array[Sprite2D]
 
-func _ready():
-	PlayerProgess.level_completed.connect(colorTile)
 
-func colorTile(num):
-	indicators[num].modulate = Color(0,1,0)
+func _on_level_manager_level_completed(index):
+	indicators[index].modulate = Color(0,1,0)
